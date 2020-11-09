9b7f4acb23ffe5906d65fa6f9ff75d9f3d4b752e
AdiAharonov <adi.ahronov@gmail.com>
2020-11-09 19:49:37 +0200

    Updates

---
+++ precache-manifest.3f3c474d5c5d8228e7e147c01191bd9a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5055dfbd9d9a2b0c7d98f6fc5a7cb30",
+    "url": "/Protfolio/index.html"
+  },
+  {
+    "revision": "fb5b475b8b7b37573605",
+    "url": "/Protfolio/static/css/main.90714f66.chunk.css"
+  },
+  {
+    "revision": "94633f33084c11bacd80",
+    "url": "/Protfolio/static/js/2.1625b45a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Protfolio/static/js/2.1625b45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5b475b8b7b37573605",
+    "url": "/Protfolio/static/js/main.275c8487.chunk.js"
+  },
+  {
+    "revision": "a60f0c17400fd5f054c0",
+    "url": "/Protfolio/static/js/runtime-main.cf4cb087.js"
+  },
+  {
+    "revision": "4b02ffaf6cdb2173fa2233b4530d8226",
+    "url": "/Protfolio/static/media/1.4b02ffaf.svg"
+  },
+  {
+    "revision": "c403763113deea49d4fccc6eb7555980",
+    "url": "/Protfolio/static/media/10.c4037631.svg"
+  },
+  {
+    "revision": "f1bd3384f7859ab9f9e49e7bb771a3f9",
+    "url": "/Protfolio/static/media/11.f1bd3384.svg"
+  },
+  {
+    "revision": "fa47cab6c97539494579d424a681fc71",
+    "url": "/Protfolio/static/media/12.fa47cab6.svg"
+  },
+  {
+    "revision": "e0b7ddb1fa16ca3b28a89e2f1e478e8c",
+    "url": "/Protfolio/static/media/13.e0b7ddb1.svg"
+  },
+  {
+    "revision": "21aeb377db2abe1113334628c410de81",
+    "url": "/Protfolio/static/media/14.21aeb377.svg"
+  },
+  {
+    "revision": "20d18cbad5110506044155924f546793",
+    "url": "/Protfolio/static/media/2.20d18cba.svg"
+  },
+  {
+    "revision": "371c05baed8b083337284313a8ab580e",
+    "url": "/Protfolio/static/media/3.371c05ba.svg"
+  },
+  {
+    "revision": "4ada9e2653f4c782e0a0e64d172bd67b",
+    "url": "/Protfolio/static/media/4.4ada9e26.svg"
+  },
+  {
+    "revision": "64de255da5de4f2600b4d910ada21dc6",
+    "url": "/Protfolio/static/media/5.64de255d.svg"
+  },
+  {
+    "revision": "8c66fe42c8e71e9fa169aa3d6a07271a",
+    "url": "/Protfolio/static/media/6.8c66fe42.svg"
+  },
+  {
+    "revision": "68eac59b4ed7c6d63e79f4d173de819b",
+    "url": "/Protfolio/static/media/7.68eac59b.svg"
+  },
+  {
+    "revision": "299c5cb08dc447edbbfe4fb1a73f0151",
+    "url": "/Protfolio/static/media/8.299c5cb0.svg"
+  },
+  {
+    "revision": "76c6bec29e9a7813a8ae75f9b1cfed1c",
+    "url": "/Protfolio/static/media/9.76c6bec2.svg"
+  },
+  {
+    "revision": "58e4633f72e3feca1e4fcf9f32d2217c",
+    "url": "/Protfolio/static/media/IBM-Bold.58e4633f.ttf"
+  },
+  {
+    "revision": "bfc0c1efd48c7e6dcbd2504c849a3a51",
+    "url": "/Protfolio/static/media/IBM-Regular.bfc0c1ef.ttf"
+  },
+  {
+    "revision": "8304e81f4386d135e63c9224e01a3e67",
+    "url": "/Protfolio/static/media/animated.8304e81f.png"
+  },
+  {
+    "revision": "63fbbcb927dde743528bf8fd37dd308a",
+    "url": "/Protfolio/static/media/arrow-btn.63fbbcb9.svg"
+  },
+  {
+    "revision": "b3d6b2573d3077b5b504457660fe8653",
+    "url": "/Protfolio/static/media/bitcoin.b3d6b257.gif"
+  },
+  {
+    "revision": "81cd217e4a8160a930c6d5fb8d1e8e82",
+    "url": "/Protfolio/static/media/clean.81cd217e.ttf"
+  },
+  {
+    "revision": "9016afe1fb833a840370df02ca5ab8d5",
+    "url": "/Protfolio/static/media/g1.9016afe1.svg"
+  },
+  {
+    "revision": "b39a9e3d5559a73273d92c960a2844cd",
+    "url": "/Protfolio/static/media/g2.b39a9e3d.svg"
+  },
+  {
+    "revision": "17277d1c1224bba1186d1c9c4438c334",
+    "url": "/Protfolio/static/media/g3.17277d1c.svg"
+  },
+  {
+    "revision": "e86fc40e1825f8d9976aea93549b7fdd",
+    "url": "/Protfolio/static/media/logo.e86fc40e.svg"
+  },
+  {
+    "revision": "12d9c79ba9f070a772f0c3f7155227d9",
+    "url": "/Protfolio/static/media/real.12d9c79b.png"
+  },
+  {
+    "revision": "053592481e889e3609bac8796b209af2",
+    "url": "/Protfolio/static/media/space.05359248.ttf"
+  },
+  {
+    "revision": "c33afcf1433c78580b397ad12e36e33b",
+    "url": "/Protfolio/static/media/spaceship.c33afcf1.svg"
+  },
+  {
+    "revision": "410775dc87bd50d8a7c951c9abade55d",
+    "url": "/Protfolio/static/media/venyou.410775dc.gif"
+  }
+]);
